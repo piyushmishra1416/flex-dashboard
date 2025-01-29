9c94e99243f2729c7e84c31f2c09fa4584565532
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2025-01-29 21:32:33 +0530

    feat: added main layout with responsive width

--- src/app/components/MainLayout.tsx
+++ src/app/components/MainLayout.tsx
@@ -26,9 +26,9 @@ import Image from 'next/image';
 
 
 export interface MenuItem {
-   text: string;
-   icon: JSX.Element;
-   link: string;
+  text: string;
+  icon: JSX.Element;
+  link: string;
  }
 
 
@@ -37,21 +37,21 @@ const drawerWidth = 240;
 const openedMixin = (theme: Theme): CSSObject => ({
   width: drawerWidth,
   transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.enteringScreen,
+   easing: theme.transitions.easing.sharp,
+   duration: theme.transitions.duration.enteringScreen,
   }),
   overflowX: "hidden",
 });
 
 const closedMixin = (theme: Theme): CSSObject => ({
   transition: theme.transitions.create("width", {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
+   easing: theme.transitions.easing.sharp,
+   duration: theme.transitions.duration.leavingScreen,
   }),
   overflowX: "hidden",
   width: `calc(${theme.spacing(7)} + 1px)`,
   [theme.breakpoints.up("sm")]: {
-    width: `calc(${theme.spacing(8)} + 1px)`,
+   width: `calc(${theme.spacing(8)} + 1px)`,
   },
 });
 
@@ -79,21 +79,22 @@ const AppBar = styled(MuiAppBar, {
 })<AppBarProps>(({ theme }) => ({
   zIndex: theme.zIndex.drawer + 1,
   transition: theme.transitions.create(["width", "margin"], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
+   easing: theme.transitions.easing.sharp,
+   duration: theme.transitions.duration.leavingScreen,
   }),
+  boxShadow: "none", // Remove shadow
   variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(["width", "margin"], {
-          easing: theme.transitions.easing.sharp,
-          duration: theme.transitions.duration.enteringScreen,
-        }),
-      },
+   {
+    props: ({ open }) => open,
+    style: {
+      marginLeft: drawerWidth,
+      width: `calc(100% - ${drawerWidth}px)`,
+      transition: theme.transitions.create(["width", "margin"], {
+       easing: theme.transitions.easing.sharp,
+       duration: theme.transitions.duration.enteringScreen,
+      }),
     },
+   },
   ],
 }));
 
@@ -105,20 +106,20 @@ const Drawer = styled(MuiDrawer, {
   whiteSpace: "nowrap",
   boxSizing: "border-box",
   variants: [
-    {
-      props: ({ open }) => open,
-      style: {
-        ...openedMixin(theme),
-        "& .MuiDrawer-paper": openedMixin(theme),
-      },
+   {
+    props: ({ open }) => open,
+    style: {
+      ...openedMixin(theme),
+      "& .MuiDrawer-paper": openedMixin(theme),
     },
-    {
-      props: ({ open }) => !open,
-      style: {
-        ...closedMixin(theme),
-        "& .MuiDrawer-paper": closedMixin(theme),
-      },
+   },
+   {
+    props: ({ open }) => !open,
+    style: {
+      ...closedMixin(theme),
+      "& .MuiDrawer-paper": closedMixin(theme),
     },
+   },
   ],
 }));
 
@@ -127,163 +128,163 @@ const MiniDrawer: React.FC<MiniDrawerProps> = ({ menuItems, content, padding, on
   const [open, setOpen] = React.useState(false);
 
   const handleDrawerOpen = () => {
-   setOpen(true);
+  setOpen(true);
  };
 
  const handleDrawerClose = () => {
-   setOpen(false);
+  setOpen(false);
  };
 
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        open={open}
-        sx={{
-          backgroundColor: "#ffffff",
-          "& .MuiDrawer-paper": {
-            backgroundColor: "#ffffff",
+   <Box sx={{ display: "flex" }}>
+    <CssBaseline />
+    <AppBar
+      position="fixed"
+      open={open}
+      sx={{
+       backgroundColor: "#ffffff",
+       "& .MuiDrawer-paper": {
+        backgroundColor: "#ffffff",
+       },
+       color: "black",
+      }}
+    >
+      <Toolbar>
+       <IconButton
+        color="inherit"
+        aria-label="open drawer"
+        onClick={handleDrawerOpen}
+        edge="start"
+        sx={[
+          {
+           marginRight: 5,
           },
-          color: "black",
-        }}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
+          open && { display: "none" },
+        ]}
+       >
+        <MenuIcon />
+       </IconButton>
+       <Typography variant="h6" fontWeight={600}  noWrap component="div">
+        Manage Invoices
+       </Typography>
+       <Box sx={{ flexGrow: 1 }} />
+       <Box sx={{ display: { xs: "none", md: "flex" } }}>
+        <IconButton
+          size="large"
+          aria-label="show 17 new notifications"
+          color="inherit"
+        >
+          <Badge badgeContent={17} color="error">
+           <NotificationsIcon />
+          </Badge>
+        </IconButton>
+        <IconButton
+          size="large"
+          edge="end"
+          aria-label="account of current user"
+          color="inherit"
+        >
+          <Image src="/man_icon.png" alt="user" width={28} height={28} />
+        </IconButton>
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', margin: 2 }}>
+          <Typography variant="body1" fontWeight={600} noWrap>
+          Rohit Sharma
+          </Typography>
+          <Typography variant="body2" fontSize="small" noWrap>
+          rohithsharma@gmail.com
+          </Typography>
+        </Box>
+       </Box>
+      </Toolbar>
+    </AppBar>
+    <Drawer
+      variant="permanent"
+      open={open}
+      sx={{
+       backgroundColor: "#ffffff",
+       "& .MuiDrawer-paper": {
+        backgroundColor: "#ffffff",
+        
+       },
+      }}
+    >
+      <DrawerHeader>
+       <Image src="/finify_logo.png" alt="Karunai Logo" className="ml-2" width={84} height={84} />
+       <Box sx={{ flexGrow: 1 }} />
+       <IconButton onClick={handleDrawerClose}>
+        {theme.direction === "rtl" ? (
+          <ChevronRightIcon />
+        ) : (
+          <ChevronLeftIcon />
+        )}
+       </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+       {menuItems.map(({ text, icon, link }) => (
+        <ListItem key={text} onClick={() => onMenuItemClick(link)} disablePadding sx={{ display: "block" }}>
+          <ListItemButton
+           sx={[
+            {
+              color: "black",
+              minHeight: 48,
+              px: 2.5,
+            },
+            open
+              ? {
+                justifyContent: "initial",
+               }
+              : {
+                justifyContent: "center",
+               },
+           ]}
+          >
+           <ListItemIcon
             sx={[
               {
-                marginRight: 5,
+               minWidth: 0,
+               justifyContent: "center",
               },
-              open && { display: "none" },
+              open
+               ? {
+                  mr: 3,
+                }
+               : {
+                  mr: "auto",
+                },
             ]}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" fontWeight={600}  noWrap component="div">
-            Manage Invoices
-          </Typography>
-          <Box sx={{ flexGrow: 1 }} />
-          <Box sx={{ display: { xs: "none", md: "flex" } }}>
-            <IconButton
-              size="large"
-              aria-label="show 17 new notifications"
-              color="inherit"
-            >
-              <Badge badgeContent={17} color="error">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-            <IconButton
-              size="large"
-              edge="end"
-              aria-label="account of current user"
-              color="inherit"
-            >
-              <Image src="/man_icon.png" alt="user" width={28} height={28} />
-            </IconButton>
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', margin: 2 }}>
-              <Typography variant="body1" fontWeight={600} noWrap>
-              Rohit Sharma
-              </Typography>
-              <Typography variant="body2" fontSize="small" noWrap>
-              rohithsharma@gmail.com
-              </Typography>
-            </Box>
-          </Box>
-        </Toolbar>
-      </AppBar>
-      <Drawer
-        variant="permanent"
-        open={open}
-        sx={{
-          backgroundColor: "#ffffff",
-          "& .MuiDrawer-paper": {
-            backgroundColor: "#ffffff",
-            
-          },
-        }}
-      >
-        <DrawerHeader>
-          <Image src="/finify_logo.png" alt="Karunai Logo" className="ml-2" width={84} height={84} />
-          <Box sx={{ flexGrow: 1 }} />
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === "rtl" ? (
-              <ChevronRightIcon />
-            ) : (
-              <ChevronLeftIcon />
-            )}
-          </IconButton>
-        </DrawerHeader>
-        <Divider />
-        <List>
-          {menuItems.map(({ text, icon, link }) => (
-            <ListItem key={text} onClick={() => onMenuItemClick(link)} disablePadding sx={{ display: "block" }}>
-              <ListItemButton
-                sx={[
-                  {
-                    color: "black",
-                    minHeight: 48,
-                    px: 2.5,
-                  },
-                  open
-                    ? {
-                        justifyContent: "initial",
-                      }
-                    : {
-                        justifyContent: "center",
-                      },
-                ]}
-              >
-                <ListItemIcon
-                  sx={[
-                    {
-                      minWidth: 0,
-                      justifyContent: "center",
-                    },
-                    open
-                      ? {
-                          mr: 3,
-                        }
-                      : {
-                          mr: "auto",
-                        },
-                  ]}
-                >
-                  {icon}
-                </ListItemIcon>
-                <ListItemText
-                  primary={text}
-                  sx={[
-                    {
-                      color: "black",
-                      fontFamily: "Poppins, sans-serif",
-                      fontSize: "1.5rem",
-                    },
-                    open
-                      ? {
-                          opacity: 1,
-                        }
-                      : {
-                          opacity: 0,
-                        },
-                  ]}
-                />
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
+           >
+            {icon}
+           </ListItemIcon>
+           <ListItemText
+            primary={text}
+            sx={[
+              {
+               color: "black",
+               fontFamily: "Poppins, sans-serif",
+               fontSize: "1.5rem",
+              },
+              open
+               ? {
+                  opacity: 1,
+                }
+               : {
+                  opacity: 0,
+                },
+            ]}
+           />
+          </ListItemButton>
+        </ListItem>
+       ))}
+      </List>
 
-       
-      </Drawer>
-      <Box component="main" sx={{ flexGrow: 1, p: padding }}>
-        <DrawerHeader />
-        {content}
-      </Box>
+     
+    </Drawer>
+    <Box component="main" sx={{ flexGrow: 1, p: padding , width: `calc(100vw - ${drawerWidth}px)`}}>
+      <DrawerHeader />
+      {content}
     </Box>
+   </Box>
   );
 }
 export default MiniDrawer;
