2f1918321a605482eb29a3081db0d1ab620a304b
Piyush <91911367+piyushmishra1416@users.noreply.github.com.>
2025-01-30 03:55:04 +0530

    build: updated eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,20 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
 
-const eslintConfig = {
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  rules: {
-    "no-var": "off",
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript'],
+  }),
+  {
+    rules: {
+      'no-var': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
   },
-};
+]
 
-export default eslintConfig;
+export default eslintConfig
